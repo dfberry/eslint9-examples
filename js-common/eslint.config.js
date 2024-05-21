1e81e5f4578f7b60ded978058b8310291a7a3e17
Dina Berry (MSFT) <diberry@microsoft.com>
2024-05-21 19:32:44 +0000

    js-common + devcontainer

---
+++ js-common/eslint.config.js
@@ -0,0 +1,14 @@
+const globals = require("globals");
+const pluginJs = require("@eslint/js");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+
+module.exports = [
+  {
+    files: ["src/**/*.js"],
+    ignores: ["*.config.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.all, /* js.configs.all or pluginJs.configs.recommended */
+  eslintPluginPrettierRecommended,
+];
